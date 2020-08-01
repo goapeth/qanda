16b4fb0836fdb03a933fc4e5a4bd15e71eb488b1
goapeth <hashimototakuo@gmail.com>
2020-08-01 17:19:36 +0900

    bootstrapの導入

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Question.create(id: 1, name: "test questions 1", content: 'test content1')
+Question.create(id: 2, name: "test questions 2", content: 'test content2')
+
+Question.create(id: 3, name: "test questions 3", content: 'test content3')
+
